200c52093db279b8583880c17f8ae40854674390
Thiago Mendonça <thiagomendonca.2801@gmail.com>
2018-10-19 14:54:59 -0300

    Inclusão de login

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,55 @@
-from flask import render_template, request, redirect
-from app import app
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from flask_login import current_user, login_user, login_required
+from app.models import User
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def root():
+    if current_user.is_authenticated:
+        return redirect('/index')
+    else:
+        return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        if current_user.is_authenticated:
+            return redirect('/index')
+        else:
+            return render_template('login.html')
     else:
         dre = request.form['DRE']
         senha = request.form['senha']
-        if dre=='1180' and senha=='123':
+        user = User.query.filter_by(dre=dre).first()
+        if user is None or not user.check_password(senha):
+            return render_template('login.html', msg='DRE ou senha inválidos')
+        else:
+            login_user(user)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
             return redirect('/index')
         else:
-            return render_template('login.html', msg='Informações incorretas!')
+            return render_template('cadastro.html')
+    else:
+        usuario = request.form['usuario']
+        email = request.form['email']
+        dre = request.form['DRE']
+        senha = request.form['senha_1']
+        user = User(username=usuario, email=email, dre=dre)
+        user.set_password(senha)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
 
-
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
 
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
