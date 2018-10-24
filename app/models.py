f132d590d65467aec262836920adf63966437d6a
Thiago Mendonça <thiagomendonca.2801@gmail.com>
2018-10-24 00:38:08 -0300

    Final

--- app/models.py
+++ app/models.py
@@ -8,16 +8,42 @@ def load_user(id):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    usuario = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     dre = db.Column(db.String(9), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Integer, index=True) #Se for 1, admin, se 0 nao
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.usuario)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def setAdmin(self):
+        self.admin = 1
+    
+    def removeAdmin():
+        self.admin = 0
+
+class Horario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    horario = db.Column(db.String(64))
+    quantidade = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Horario {}>'.format(self.horario)
+
+    def removeVaga(self):
+        self.quantidade = self.quantidade - 1
+
+class Agendamento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    horario_id = db.Column(db.Integer, db.ForeignKey('horario.id'))
+
+    def __repr__(self):
+        return '<Agendamento {}>'.format(self.horario_id)
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from flask_login import current_user, login_user, login_required
-from app.models import User
+from app.models import User, Horario, Agendamento
+import datetime
 
 @app.route('/')
 def root():
     if current_user.is_authenticated:
-        return redirect('/index')
+        return redirect('/home')
     else:
         return redirect('/login')
 
@@ -14,7 +15,7 @@ def root():
 def login():
     if request.method == 'GET':
         if current_user.is_authenticated:
-            return redirect('/index')
+            return redirect('/home')
         else:
             return render_template('login.html')
     else:
@@ -27,14 +28,14 @@ def login():
             login_user(user)
             next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('index')
+                next_page = url_for('home')
             return redirect(next_page)
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     if request.method == 'GET':
         if current_user.is_authenticated:
-            return redirect('/index')
+            return redirect('/home')
         else:
             return render_template('cadastro.html')
     else:
@@ -42,14 +43,58 @@ def cadastro():
         email = request.form['email']
         dre = request.form['DRE']
         senha = request.form['pass']
-        user = User(username=usuario, email=email, dre=dre)
+        user = User(usuario=usuario, email=email, dre=dre, admin=0)
         user.set_password(senha)
         db.session.add(user)
         db.session.commit()
         return redirect('/login')
 
 
-@app.route('/index')
+@app.route('/home')
 @login_required
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/agendamento', methods=['GET', 'POST'])
+@login_required
+def agendamento():
+    if request.method == 'GET':
+        return render_template('agendamento.html')
+    else:
+        checaagend = Agendamento.query.filter_by(user_id=current_user.id).first()
+        if not checaagend is None:
+            return render_template('agendamento.html', msg='Você já agendou!') 
+        horarioselecionado = request.form['horario']
+        horario = Horario.query.filter_by(horario=horarioselecionado).first()
+        horario.removeVaga()
+        agendamento = Agendamento(user_id=current_user.id, horario_id=horario.id)
+        db.session.add(agendamento)
+        db.session.commit()
+        return render_template('agendamento.html', msg='Agendado com sucesso!')
+
+@app.route('/admin')
+@login_required
+def admin():
+    if request.args.get('dre') is None:
+        agendamentos = Agendamento.query.all()
+        agends = []
+        for a in agendamentos:
+            user = User.query.filter_by(id=a.user_id).first()
+            hora = Horario.query.filter_by(id=a.horario_id).first()
+            agends = agends + [ { 'dre': user.dre, 'horario': hora.horario }]
+        agora = str(datetime.datetime.now().hour) + ':' + str(datetime.datetime.now().minute)
+        msg = 0
+        if datetime.datetime.now().minute > 0:
+            h = Horario.query.filter_by(horario=str(datetime.datetime.now().hour) + ':00').first()
+            if h is None:
+                msg = 0
+            else:
+                a = Agendamento.query.filter_by(horario_id=h.id)
+                msg = len(a)
+        return render_template('admin.html', agends=agends, msg=msg)
+    else:
+        userdre = request.args.get('dre')
+        user = User.query.filter_by(dre=userdre).first()
+        db.session.delete(Agendamento.query.filter_by(user_id=user.id).first())
+        db.session.commit()
+        return redirect('/admin')
